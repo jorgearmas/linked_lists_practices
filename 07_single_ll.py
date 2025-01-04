54e0e021a70956497f5bc8fb5d342796096d62d8
Jorge Armas <jorgearmasc0496@gmail.com>
2025-01-04 14:34:49 -0600

    LL: create, insert and delete last node

---
+++ 07_single_ll.py
@@ -0,0 +1,51 @@
+# Write a Python program to delete the last item from a singly linked list.
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+class LinkedList:
+    def __init__(self, value):
+        self.head_node = Node(value)
+    
+    def insert_beginning(self, value):
+        new_node = Node(value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def delete_last_item(self):
+        current_node = self.head_node
+        prev_node = self.head_node
+        while current_node:
+            if current_node.get_next_node().get_next_node() is None:
+                current_node.set_next_node(None)
+                break
+            current_node = current_node.get_next_node()
+            
+
+    def print_list(self):
+        current_node = self.head_node
+        while current_node:
+            print(current_node.get_value())
+            current_node = current_node.get_next_node()
+
+my_linked_list = LinkedList(5)
+my_linked_list.insert_beginning(10)
+my_linked_list.insert_beginning(15)
+my_linked_list.insert_beginning(20)
+my_linked_list.insert_beginning(25)
+my_linked_list.insert_beginning(30)
+my_linked_list.insert_beginning(35)
+
+my_linked_list.delete_last_item()
+my_linked_list.print_list()
+
