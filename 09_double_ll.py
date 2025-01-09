bc85c027f50e1f30a1f22304e4b9d4b0085b82ea
Jorge Armas <jorgearmasc0496@gmail.com>
2025-01-09 08:53:36 -0600

    DLL: create, insert head, insert tail and iterate foward

---
+++ 09_double_ll.py
@@ -0,0 +1,70 @@
+# Write a Python program to create a doubly linked list, append some items and iterate through the list (print forward).
+
+class Node:
+    def __init__(self, value, prev_node=None, next_node=None):
+        self.value = value
+        self.prev_node = prev_node
+        self.next_node = next_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_prev_node(self):
+        return self.prev_node
+    
+    def set_prev_node(self, prev_node):
+        self.prev_node = prev_node
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head_node = None
+        self.tail_node = None
+    
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node
+
+        if current_head is not None:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+
+        self.head_node = new_head
+
+        if self.tail_node is None:
+            self.tail_node = new_head
+    
+    def add_to_tail(self, new_value):
+        new_tail = Node(new_value)
+        current_tail = self.tail_node
+
+        if current_tail is not None:
+            new_tail.set_prev_node(new_tail)
+            current_tail.set_next_node(new_tail)
+        
+        self.tail_node = new_tail
+
+        if self.head_node is None:
+            self.head_node = new_tail
+    
+    def print_foward_linked_list(self):
+        current_node = self.head_node
+
+        while current_node is not None:
+            print(current_node.get_value())
+            current_node = current_node.get_next_node()
+
+dll = DoublyLinkedList()
+dll.add_to_head(10)
+dll.add_to_head(20)
+dll.add_to_head(30)
+dll.add_to_tail(100)
+dll.add_to_tail(200)
+dll.add_to_tail(300)
+
+dll.print_foward_linked_list()
